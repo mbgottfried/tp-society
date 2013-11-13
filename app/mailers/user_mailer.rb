6bbc4cb58132dac944bf7a2b4f74967fc60e2062
shagmbg <michael.b.gottfried@gmail.com>
2013-11-13 15:13:33 -0500

    successfully set up password reset mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "concierge@toiletpapersociety.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
  
   def password_reset(user)
     @user = user
-    mail :to => user.email, :subject => "Password Reset"
+    mail :to => user.email, :subject => "Toilet Paper Society Password Reset"
   end
 
 end
