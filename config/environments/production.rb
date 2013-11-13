0dcd631403151ee040da6c3e090388e4d47f0a7b
shagmbg <michael.b.gottfried@gmail.com>
2013-11-13 14:56:57 -0500

    added mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,13 @@ Monospace::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  config.action_mailer.default_url_options = { :host => "http://tp-society.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.mandrillapp.com",
+  :port                 => 587,
+  :domain               => 'heroku.com',
+  :user_name            => ENV['MANDRILL_USERNAME'],
+  :password             => ENV['MANDRILL_APIKEY'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
