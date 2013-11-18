fda378c2df86c920115ecd0e1db1395b252cb308
shagmbg <michael.b.gottfried@gmail.com>
2013-11-18 14:07:45 -0500

    changed to event.data.object.customer with customer.updated

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@
 
   StripeEvent.setup do
     subscribe 'customer.updated' do |event|
-    	user = User.find_by_stripe_id(event.data.object.card.customer)
+    	user = User.find_by_stripe_id(event.data.object.customer)
     	user.most_recent_charge = DateTime.new
     	user.save
     end
