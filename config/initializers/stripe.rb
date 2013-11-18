edf5d0f961299267e2254986b6fabfd15a04ea51
shagmbg <michael.b.gottfried@gmail.com>
2013-11-18 13:27:16 -0500

    changed to stripe_id

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@
 
   StripeEvent.setup do
     subscribe 'charge.succeeded' do |event|
-    	@user = User.find(User.find_by_stripe_customer_token(data['data']['object']['customer']).user)
+    	@user = User.find(User.find_by_stripe_id(data['data']['object']['customer']).user)
     	@user.most_recent_charge = DateTime.new
     	@user.save
     end
