e7edb2f495caf584acf3ed38b1b35393e9eecb30
shagmbg <michael.b.gottfried@gmail.com>
2013-11-18 14:52:08 -0500

    added code to webhooks controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,2 +1,9 @@
 class WebhooksController < ApplicationController  
+	StripeEvent.setup do
+    subscribe 'customer.updated' do |event|
+    	user = User.find_by_stripe_id(event.data.object.customer)
+    	user.most_recent_charge = DateTime.new
+    end
+
+  end
 end  	
