e44c3ab87d42b8828f6e0f6fd15ca8e75b36108e
shagmbg <michael.b.gottfried@gmail.com>
2013-11-18 14:27:50 -0500

    added exclamation point

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,7 +5,7 @@
     subscribe 'customer.updated' do |event|
     	user = User.find_by_stripe_id(event.data.object.customer)
     	user.most_recent_charge = DateTime.new
-    	user.save
+    	user.save!
     end
 
   end
