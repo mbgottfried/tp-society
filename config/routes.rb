17b50064177775aedbce7befac61dfe4fed7f97d
shagmbg <michael.b.gottfried@gmail.com>
2013-11-19 16:45:30 -0500

    removed controller from routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Monospace::Application.routes.draw do
   resources :users
   resources :sessions
   resources :password_resets
-  resources :webhooks_controller
+  resources :webhooks
 
   get "plans" => "pages#plans"
   get "orders" => "admin#orders"
