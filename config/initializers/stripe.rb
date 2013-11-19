d5dd63334c15d4b2347c237020eba620ec1c30cb
shagmbg <michael.b.gottfried@gmail.com>
2013-11-19 12:06:16 -0500

    changed customer to id when identifying user

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@
 
   StripeEvent.setup do
     subscribe 'customer.updated' do |event|
-    	user = User.find_by_stripe_id(event.data.object.customer)
+    	user = User.find_by_stripe_id(event.data.object.id)
     	user.custup
     end
   end
