721dbbcee2ae9c928463ea0c11f64893c5fca6c8
shagmbg <michael.b.gottfried@gmail.com>
2013-11-19 12:00:01 -0500

    webhooks successful, changed back to event.data.object.customer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@
 
   StripeEvent.setup do
     subscribe 'customer.updated' do |event|
-    	user = User.find_by_stripe_id("cus_2wldOugaiiuBlj")
+    	user = User.find_by_stripe_id(event.data.object.customer)
     	user.custup
     end
   end
