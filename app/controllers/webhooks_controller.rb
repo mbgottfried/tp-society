d339bd1bb98cac73328733e9f0092c6f7ea0a41e
shagmbg <michael.b.gottfried@gmail.com>
2013-11-19 12:38:45 -0500

    cleaned up easypost formatting

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,14 +1,17 @@
 class WebhooksController < ApplicationController  
 	
+	require 'easypost'
+	EasyPost.api_key = 'KCt9J7ZWaxjY01LgHSDs0A'
+
 	def charge_succeeded
 		Easypost::Address.create(
-			:name => user.name
-			:street1 => user.street1
-			:street2 => user.street2
-			:city => user.city
-			:state => user.state
-			:zip => user.zip
-			:country = 'USA'
+			:name => user.name,
+			:street1 => user.street1,
+			:street2 => user.street2,
+			:city => user.city,
+			:state => user.state,
+			:zip => user.zip,
+			:country = 'USA',
 			:email => user.email	
 		)
 	end	
