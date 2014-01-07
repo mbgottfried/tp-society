a5b39d144e52f594a092273d382cc2aa113cca4d
shagmbg <michael.b.gottfried@gmail.com>
2014-01-07 16:43:32 -0500

    replaced $ with jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,47 +2,47 @@
 //= require jquery
 //= require shrink
 
-$(function() {
-  $("#credit-card input, #credit-card select").attr("disabled", false);
+jQuery(function() {
+  jQuery("#credit-card input, #credit-card select").attr("disabled", false);
 
-  $("form:has(#credit-card)").submit(function() {
+  jQuery("form:has(#credit-card)").submit(function() {
     var form = this;
-    $("#user_submit").attr("disabled", true);
-    $("#credit-card input, #credit-card select").attr("name", "");
-    $("#credit-card-errors").hide();
+    jQuery("#user_submit").attr("disabled", true);
+    jQuery("#credit-card input, #credit-card select").attr("name", "");
+    jQuery("#credit-card-errors").hide();
 
-    if (!$("#credit-card").is(":visible")) {
-      $("#credit-card input, #credit-card select").attr("disabled", true);
+    if (!jQuery("#credit-card").is(":visible")) {
+      jQuery("#credit-card input, #credit-card select").attr("disabled", true);
       return true;
     }
     
     var card = {
-      number:   $("#credit_card_number").val(),
-      expMonth: $("#_expiry_date_2i").val(),
-      expYear:  $("#_expiry_date_1i").val(),
-      cvc:      $("#cvv").val()
+      number:   jQuery("#credit_card_number").val(),
+      expMonth: jQuery("#_expiry_date_2i").val(),
+      expYear:  jQuery("#_expiry_date_1i").val(),
+      cvc:      jQuery("#cvv").val()
     };
 
 
     Stripe.createToken(card, function(status, response) {
       if (status === 200) {
-        $("#user_last_4_digits").val(response.card.last4);
-        $("#user_stripe_token").val(response.id);
+        jQuery("#user_last_4_digits").val(response.card.last4);
+        jQuery("#user_stripe_token").val(response.id);
         form.submit();
       } else {
-        $("#stripe-error-message").text(response.error.message);
-        $("#credit-card-errors").show();
-        $("#user_submit").attr("disabled", false);
+        jQuery("#stripe-error-message").text(response.error.message);
+        jQuery("#credit-card-errors").show();
+        jQuery("#user_submit").attr("disabled", false);
       }
     });
 
     return false;
   });
 
-  $("#change-card a").click(function() {
-    $("#change-card").hide();
-    $("#credit-card").show();
-    $("#credit_card_number").focus();
+  jQuery("#change-card a").click(function() {
+    jQuery("#change-card").hide();
+    jQuery("#credit-card").show();
+    jQuery("#credit_card_number").focus();
     return false;
   });
 });
