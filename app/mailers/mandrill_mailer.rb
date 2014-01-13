a82df04a126b160583364acac7a4b7e838365976
shagmbg <michael.b.gottfried@gmail.com>
2014-01-13 15:55:38 -0500

    trying to figure out mandrillll

--- app/mailers/mandrill_mailer.rb
+++ app/mailers/mandrill_mailer.rb
@@ -2,9 +2,10 @@ class TransactionMailer < MandrillMailer::TemplateMailer
   default from: 'conceirge@toiletpapersociety.com'
 
   def mandrill_welcome(user)
-    mandrill_mail template: 'Toilet Paper Society - Welcome'
     @user = user
-    mail :to => user.email, :subject => "Welcome to the Society!"
+    mandrill_mail template: 'Toilet Paper Society - Welcome',
+    to: user.email, 
+    subject: "Welcome to the Society!"
   end
 
 end
