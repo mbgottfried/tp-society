e83d475f696e16388653f860424909bb65c2e1ab
shagmbg <michael.b.gottfried@gmail.com>
2014-01-14 14:18:14 -0500

    added an email for new subscriptions to us

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,6 +5,13 @@
 #EasyPost.api_key = 'KCt9J7ZWaxjY01LgHSDs0A'
 
   StripeEvent.setup do
+    subscribe 'customer.subscription.created'
+      user = User.find_by_stripe_id(event.data.object.customer)
+      user.subcreate
+    end
+  end
+
+  StripeEvent.setup do
     subscribe 'customer.updated' do |event|
     	user = User.find_by_stripe_id(event.data.object.id)
     	user.custup
